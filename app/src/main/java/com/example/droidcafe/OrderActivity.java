740211e4b3231ee969b437974bbafb1b5da58779
VictorDominguez5348 <victordominguez5348@outlook.com>
2020-02-11 08:16:46 -0800

    fixed issue with the extraction of links

--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class OrderActivity extends AppCompatActivity
-{
+public class OrderActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
+    }
 }
